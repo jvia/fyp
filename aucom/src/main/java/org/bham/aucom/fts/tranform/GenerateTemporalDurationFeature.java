4898f86818e2e0bf992e62bb3bf916092d6a52e5
Jeremiah Via <jeremiah.via@gmail.com>
2012-02-12 15:22:07 +0000

    Documented GenerateTemporalDuration feature class.

--- aucom/src/main/java/org/bham/aucom/fts/tranform/GenerateTemporalDurationFeature.java
+++ aucom/src/main/java/org/bham/aucom/fts/tranform/GenerateTemporalDurationFeature.java
@@ -5,20 +5,37 @@ import org.bham.aucom.data.TemporalDurationFeature;
 
 import java.util.logging.Logger;
 
+/**
+ * This node transforms system events into its {@link TemporalDurationFeature}
+ * representation.
+ *
+ * @author Raphael Golombek <rgolombe@cor-lab.uni-bielefeld.de>
+ * @author Jeremiah M. Via <jxv911@cs.bham.ac.uk
+ */
 public class GenerateTemporalDurationFeature extends AbstractAucomTranformNode<DataType, TemporalDurationFeature> {
-
-    private TemporalDurationFeatureGenerator generator;
     private final transient Logger log = Logger.getLogger(getClass().getName());
+    private TemporalDurationFeatureGenerator generator;
 
+    /**
+     * Create the transformation node.
+     */
     public GenerateTemporalDurationFeature() {
+        // TODO :: see if gnerator can be passed in as constructor parameter
         super("GenerateDurationFeature");
         generator = null;
     }
 
+    /**
+     * Transforms system input into a {@link TemporalDurationFeature}.
+     *
+     * @param input the system event
+     * @return the temporal duration feature
+     * @throws Exception something went wrong
+     */
     @Override
     protected TemporalDurationFeature iTransform(DataType input) throws Exception {
         if (generator == null) {
-            log.severe("Generator is missing, cannot generate TemporalDurationFeature ");
+            log.severe("Generator is missing. Cannot generate TemporalDurationFeature ");
             return null;
         }
 
@@ -29,16 +46,30 @@ public class GenerateTemporalDurationFeature extends AbstractAucomTranformNode<D
         return feature;
     }
 
+    /**
+     * Resets the {@link TemporalDurationFeatureGenerator}.
+     */
     public void reset() {
         if (getGenerator() != null)
             getGenerator().initializeLastOccurances();
     }
 
+    /**
+     * Set the generator.
+     *
+     * @param generator the generator to use.
+     */
     public synchronized void setGenerator(TemporalDurationFeatureGenerator generator) {
         this.generator = generator;
     }
 
+    /**
+     * Get the generator.
+     *
+     * @return the generator.
+     */
     public synchronized TemporalDurationFeatureGenerator getGenerator() {
+        // TODO :: does this really need to be synchronized?
         return generator;
     }
 }
