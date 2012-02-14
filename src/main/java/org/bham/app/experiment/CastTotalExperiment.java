c5fea0f330351ff38857daecf4c784dcb8fb90fb
Jeremiah Via <jeremiah.via@gmail.com>
2012-02-14 16:30:44 +0000

    Deleting unused files.

---
+++ src/main/java/org/bham/app/experiment/CastTotalExperiment.java
@@ -0,0 +1,26 @@
+package org.bham.app.experiment;
+
+/**
+ * @author Jeremiah Via <jxv911@cs.bham.ac.uk>
+ */
+public class CastTotalExperiment implements Experiment {
+    @Override
+    public void preprocess() throws Exception {
+        throw new UnsupportedOperationException("preprocess() not implemented yet");
+    }
+
+    @Override
+    public void process() throws Exception {
+        throw new UnsupportedOperationException("process() not implemented yet");
+    }
+
+    @Override
+    public void postprocess() throws Exception {
+        throw new UnsupportedOperationException("postprocess() not implemented yet");
+    }
+
+    @Override
+    public Result call() throws Exception {
+        throw new UnsupportedOperationException("call() not implemented yet");
+    }
+}
