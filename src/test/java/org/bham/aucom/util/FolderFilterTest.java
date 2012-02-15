17226baa38a3e1181ff557ec1cea87bb0ed8264e
Jeremiah Via <jeremiah.via@gmail.com>
2012-02-15 02:11:04 +0000

    Added FolderFilter test.

--- src/test/java/org/bham/aucom/util/FolderFilterTest.java
+++ src/test/java/org/bham/aucom/util/FolderFilterTest.java
@@ -5,66 +5,27 @@
 
 package org.bham.aucom.util;
 
-import java.io.File;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.io.File;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
- *
- * @author jxv911
+ * @author Jeremiah M. Via <jxv911@cs.bham.ac.uk>
  */
 public class FolderFilterTest {
-
-    public FolderFilterTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() throws Exception {
-    }
-
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-    }
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
 
     /**
      * Test of accept method, of class FolderFilter.
      */
     @Test
     public void testAccept() {
-        System.out.println("accept");
+        FolderFilter instance = new FolderFilter();
         File f = null;
-        FolderFilter instance = new FolderFilter();
-        boolean expResult = false;
-        boolean result = instance.accept(f);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertThat(instance.accept(f), is(false));
+        f = new File("/");
+         assertThat(instance.accept(f), is(true));
     }
-
-    /**
-     * Test of getDescription method, of class FolderFilter.
-     */
-    @Test
-    public void testGetDescription() {
-        System.out.println("getDescription");
-        FolderFilter instance = new FolderFilter();
-        String expResult = "";
-        String result = instance.getDescription();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
 }
