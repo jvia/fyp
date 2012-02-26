0ca381bee9f59387f912396e5d5c852471e7845f
Jeremiah Via <jeremiah.via@gmail.com>
2012-02-26 20:11:35 +0000

    Big experiment code.

--- experiment_configs/all.sh
+++ experiment_configs/all.sh
@@ -4,28 +4,30 @@
 ORIGINAL=../target/original.jar
 REDUCED=../target/reduced.jar
 METRONOME=../target/metronome.jar
-DIR=3chain
-ITERS=1
+DIR=5chain
+ITERS=10
 # Cast configs
-NORMAL=../cast_configs/three_chain/three_chain.cast
-FAULTY=../cast_configs/three_chain/three_chain_fault.cast
+NORMAL=../cast_configs/five_chain/five_chain.cast
+FAULTY=../cast_configs/five_chain/five_chain_fault.cast
 
 mkdir $DIR ; mkdir $DIR/orig ; mkdir $DIR/reduced ; mkdir $DIR/metronome
-./observation_collection.sh -a $ORIGINAL -o orig.obs -c $NORMAL
+./observation_collection.sh -a $ORIGINAL -o obs -c $NORMAL
 
 #
 # Original
 #
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $ORIGINAL -i orig.obs -o orig.cl -c $NORMAL
+    echo "Original: normal run $i"
+    ./experiment.sh -a $ORIGINAL -i obs -o orig.cl -c $NORMAL
     ./convert.sh -a $ORIGINAL -i orig.cl -o orig.csv
     mv orig.csv "$DIR/orig/normal_$i.csv"
 done
 
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $ORIGINAL -i orig.obs -o orig.cl -c $FAULTY
+    echo "Original: faulty run $i"
+    ./experiment.sh -a $ORIGINAL -i obs -o orig.cl -c $FAULTY
     ./convert.sh -a $ORIGINAL -i orig.cl -o orig.csv
     mv orig.csv "$DIR/orig/faulty_$i.csv"
 done
@@ -35,16 +37,18 @@ done
 #
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $REDUCED -i reduced.obs -o reduced.cl -c $NORMAL
+    echo "Reduced: normal run $i"
+    ./experiment.sh -a $REDUCED -i obs -o reduced.cl -c $NORMAL
     ./convert.sh    -a $REDUCED -i reduced.cl  -o reduced.csv
     mv reduced.csv "$DIR/reduced/normal_$i.csv"
 done
 
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $REDUCED -i reduced.obs -o reduced.cl -c $FAULTY
+    echo "Reduced: faulty run $i"
+    ./experiment.sh -a $REDUCED -i obs -o reduced.cl -c $FAULTY
     ./convert.sh    -a $REDUCED -i reduced.cl  -o reduced.csv
-    mv orig.csv "$DIR/reduced/faulty_$i.csv"
+    mv reduced.csv "$DIR/reduced/faulty_$i.csv"
 done
 
 #
@@ -52,14 +56,16 @@ done
 #
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $METRONOME -i metronome.obs -o metronome.cl -c $NORMAL
+    echo "Metronome: normal run $i"
+    ./experiment.sh -a $METRONOME -i obs -o metronome.cl -c $NORMAL
     ./convert.sh    -a $METRONOME -i metronome.cl  -o metronome.csv
     mv metronome.csv "$DIR/metronome/normal_$i.csv"
 done
 
 for i in `seq 1 $ITERS`;
 do
-    ./experiment.sh -a $METRONOME -i metronome.obs -o metronome.cl -c $FAULTY
+    echo "Metronome: faulty run $i"
+    ./experiment.sh -a $METRONOME -i obs -o metronome.cl -c $FAULTY
     ./convert.sh    -a $METRONOME -i metronome.cl  -o metronome.csv
     mv metronome.csv "$DIR/metronome/faulty_$i.csv"
 done
