e62cde9bb2f9aadb5e8daa96fd11170a61341fb8
Jeremiah Via <jeremiah.via@gmail.com>
2012-04-03 11:39:30 -0500

    Add graphs.

--- docs/dissertation/init.el
+++ docs/dissertation/init.el
@@ -4,30 +4,32 @@
  '((dot        . t)
    (emacs-lisp . t)
    (org        . t)
-   (sh         . t)))
+   (sh         . t)
+   (gnuplot    . t)))
+(setq org-confirm-babel-evaluate nil)
 (add-to-list 'org-export-latex-classes
              '("dissertation"
-               "\\documentclass{article}
+"\\documentclass{article}
 \\usepackage[utf8]{inputenc}
 \\usepackage[T1]{fontenc}
 \\usepackage{geometry}
-               \\usepackage{setspace}
-               \\usepackage{wrapfig}
-               \\usepackage{graphicx}
-               \\usepackage{mathtools}
-               \\usepackage{paralist}
-               \\usepackage{longtable}
-               \\usepackage{float}
-               \\usepackage{wrapfig}
-               \\usepackage{soul}
-               \\usepackage{textcomp}
-               \\usepackage{marvosym}
-               \\usepackage{latexsym}
-               \\usepackage{amssymb}
-               \\usepackage{hyperref}
-               \\tolerance=1000
-               [NO-DEFAULT-PACKAGES]
-               [NO-PACKAGES]"
+\\usepackage{setspace}
+\\usepackage{wrapfig}
+\\usepackage{graphicx}
+\\usepackage{mathtools}
+\\usepackage{paralist}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{marvosym}
+\\usepackage{latexsym}
+\\usepackage{amssymb}
+\\usepackage{hyperref}
+\\tolerance=1000
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]"
                ("\\section{%s}" . "\\section*{%s}")
                ("\\subsection{%s}" . "\\subsection*{%s}")
                ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
